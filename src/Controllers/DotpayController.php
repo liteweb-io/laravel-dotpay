90a2d2983aa374f21189860a419b475edf08929b
axotion <axotion@linux.pl>
2017-11-20 14:47:03 +0100

    User support

--- src/Controllers/DotpayController.php
+++ src/Controllers/DotpayController.php
@@ -63,6 +63,7 @@ class DotpayController extends Controller
         $dotpayApi = new DotpayApi($config);
         $response = $dotpayApi->createPayment(new Payment($data));
 
+        $data['user'] = $request->user();
         event(new DotpayPaymentEvent(new Payment($data)));
 
         return redirect()->to($response->getPaymentUrl());
